894aecb5fa2dd1c08ae6b278b03dfd87014b9a73
mearjuntripathi <74202760+Arjuntripathi@users.noreply.github.com>
2022-07-20 11:54:38 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,19 +1,22 @@
 package main
 
 //inport for use router
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"GO/StudentInfo"
+	)
 
 
 //main function which perform every action
 
 func main() {
 	router := gin.Default()
-	router.GET("/Student", getStudent)
-	router.POST("/Student", postStudent)
-	router.GET("/Student/:id", getAStudent)
+	router.GET("/Student", StudentInfo.GetStudent)
+	router.POST("/Student", StudentInfo.PostStudent)
+	router.GET("/Student/:id", StudentInfo.GetAStudent)
 	
-	router.PUT("/Student/:id", updateAStudent)
-	router.DELETE("/Student/:id", DeleteAStudent)
+	router.PUT("/Student/:id", StudentInfo.UpdateAStudent)
+	router.DELETE("/Student/:id", StudentInfo.DeleteAStudent)
 
 	router.Run("localhost:3010")
 }
